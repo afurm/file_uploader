41b27dae11ba3e22bc4a67afc60338a895d76818
andrii <furmanets.andriy@gmail.com>
2014-06-30 12:48:14 +0300

    configured prod env & added versions

--- config.ru
+++ config.ru
@@ -1,6 +1,10 @@
  $:.unshift File.expand_path '..', __FILE__
 
  require 'lib/env'
+
  require 'lib/app'
+ require 'lib/uploaders/asset'
+ require 'lib/models/asset'
+ require 'lib/models/user'
 
  run API::App
--- lib/env.rb
+++ lib/env.rb
@@ -3,6 +3,8 @@ require 'bundler'
 Bundler.require
 Bundler.require :production
 
+Sequel.connect 'sqlite://db/db.sqlite3'
+
 CarrierWave.configure do |conf|
   conf.root = File.expand_path '../../', __FILE__
   conf.cache_dir = File.expand_path '../../public', __FILE__
